305f21202a615a3d464c9227dae86dea85a1ba9b
Ruslan Namanov <ruslan.namanov@yandex.ru>
2016-12-24 10:04:27 +0300

    правки карты для ie

--- js/map.js
+++ js/map.js
@@ -1,16 +1,11 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('google-map'), {
     zoom: 16,
-    center: {
-      lat: 59.9387942,
-      lng: 30.3230833
-    },
-
+    center: {lat: 59.9387942, lng: 30.3230833}
   });
 
   setMarkers(map);
 }
-
 
 var points = [
   ['Мы здесь!', 59.9387942, 30.3230833],
@@ -26,10 +21,7 @@ function setMarkers(map) {
   for (var i = 0; i < points.length; i++) {
     var point = points[i];
     var marker = new google.maps.Marker({
-      position: {
-        lat: point[1],
-        lng: point[2]
-      },
+      position: {lat: point[1], lng: point[2]},
       map: map,
       icon: image,
       title: point[0]
